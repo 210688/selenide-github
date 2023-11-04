cecc9d8ad5aa0472e6fc4a58ff7a5d8e4fba617c
Oleg <gordienko21061988@gmail.com>
2023-11-04 19:06:20 +0300

    lesson snippets

---
+++ src/test/java/github/ContributorsTest.java
@@ -0,0 +1,32 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.ByText;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ContributorsTest {
+
+    @Test
+    void andreiSolntcevShouldContributorsTheMost() {
+
+     // Configuration.browserSize = "1600x1080";
+        // Configuration.browser = "firefox";
+    //  Configuration.timeout = 2000;
+ //     Configuration.headless = true;
+
+        open("https://github.com/selenide/selenide");
+        $(".Layout-sidebar").$(byText("Contributors"))
+                .ancestor(".BorderGrid-cell").$$("ul li").first()
+                .hover();
+
+        $$(".Popover").find(visible).shouldHave(text("Andrei Solntsev"));
+
+
+
+    }
+}

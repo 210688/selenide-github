606046ab675a433c76b8eb938869f8259108dbec
gordienko_os <gordienko210688@gmail.com>
2025-04-03 20:13:39 +0300

    Написание новых тестов

--- src/test/java/api/ReqresTests.java
+++ src/test/java/api/ReqresTests.java
@@ -1,12 +1,17 @@
 package api;
 
 import io.restassured.RestAssured;
+import models.NameBodyModel;
+import models.NameResponseModel;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReqresTests {
 
@@ -27,14 +32,23 @@ public class ReqresTests {
 
     @Test
     void postGreateUser() {
-        given()
-                .body("{\"name\": \"morpheus\",\"job\": \"leader\"}")
+        NameBodyModel nameBodyModel = new NameBodyModel();
+        nameBodyModel.setName("morpheus");
+        nameBodyModel.setJob("leader");
+
+        NameResponseModel nameResponseModel =  given()
+                .body(nameBodyModel)
                 .contentType(JSON)
+                .when()
                 .post("/users")
                 .then()
                 .statusCode(201)
-                .body("name", is("morpheus"))
-                .body("job", is("leader"));
+                .extract().as(NameResponseModel.class);
+
+        assertEquals("morpheus", nameResponseModel.getName());
+        assertEquals("leader", nameResponseModel.getJob());
+
+
     }
 
     @Test
---
+++ src/test/java/models/NameBodyModel.java
@@ -0,0 +1,21 @@
+package models;
+
+public class NameBodyModel {
+    String name, job;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+}

911493599b1e8d341f3cd9fe60e54a916a2e7b76
gordi <gordienkoos@krlb.ru>
2025-05-27 08:11:18 +0300

    Написание новых тестов

--- src/test/java/demowebshop/LoginTests.java
+++ src/test/java/demowebshop/LoginTests.java
@@ -3,11 +3,15 @@ package demowebshop;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
 
 public class LoginTests {
 
@@ -26,6 +30,39 @@ public class LoginTests {
         step("Fill login form", () -> {
             $("#Email").setValue(login);
             $("#Password").setValue(password).pressEnter();
+        });
+        step("Verify successful autorization", () ->
+                $(byText("qa@qa.guru")).shouldHave(text(login)));
+    }
+
+    @Test
+    void loginWithApiTest() {
+//        step("Open login page", () ->
+//                open("/login"));
+//        step("Fill login form", () -> {
+//            $("#Email").setValue(login);
+//            $("#Password").setValue(password).pressEnter();
+//        });
+
+        step("Get authorization cookie by api and set it to browser", () -> {
+            String authCookieKey = "NOPCOMMERCE.AUTH";
+            String authCookieKeyValue = given()
+                    .contentType("application/x-www-form-urlencoded")
+                    .formParam("Email", login)
+                    .formParam("Password", password)
+                    .when()
+                    .post("https://demowebshop.tricentis.com/login")
+                    .then()
+                    .log().all()
+                    .statusCode(302)
+                    .extract()
+                    .cookie(authCookieKey);
+
+            open("/favicon.png");
+            Cookie authCookie = new Cookie(authCookieKey, authCookieKeyValue);
+            getWebDriver().manage().addCookie(authCookie);
+
+
         });
         step("Verify successful autorization", () ->
                 $(byText("qa@qa.guru")).shouldHave(text(login)));

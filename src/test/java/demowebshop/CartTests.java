d72947b7f2d468fc93a421b4dd6d19cbc95cdee0
gordi <gordienkoos@krlb.ru>
2025-05-30 04:54:38 +0300

    Написание новых тестов

--- src/test/java/demowebshop/CartTests.java
+++ src/test/java/demowebshop/CartTests.java
@@ -2,10 +2,25 @@ package demowebshop;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.Is.is;
 
 public class CartTests extends TestBase {
+    @Test
+    void checkTheItemInTheCart() {
+        open("https://demowebshop.tricentis.com/");
+        $(byText("Log in")).click();
+        $("#Email").setValue("gordienko210688@gmail.com");
+        $("#Password").setValue("Nokiac500");
+        $(".login-button").click();
+        $(".cart-qty").shouldHave(text("(0)"));
+
+    }
+
     @Test
     void addToCartTest() {
         String authCookieKey = "NOPCOMMERCE.AUTH";
@@ -16,7 +31,6 @@ public class CartTests extends TestBase {
                 .when()
                 .post("/login")
                 .then()
-                .log().all()
                 .statusCode(302)
                 .extract()
                 .cookie(authCookieKey);
@@ -33,28 +47,20 @@ public class CartTests extends TestBase {
                 .when()
                 .post("/addproducttocart/details/72/1")
                 .then()
-                .log().all()
                 .statusCode(200)
                 .body("success", is(true))
                 .body("message", is("The product has been added to your <a href=\"/cart\">shopping cart</a>"));
     }
 
     @Test
-    void addToCartAsAnonymTest() {
-        String data = "product_attribute_72_5_18=53" +
-                "&product_attribute_72_6_19=54" +
-                "&product_attribute_72_3_20=57&addtocart_72.EnteredQuantity=1";
-
-        given()
-                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .body(data)
-                .when()
-                .post("/addproducttocart/details/72/1")
-                .then()
-                .log().all()
-                .statusCode(200)
-                .body("success", is(true))
-                .body("message", is("The product has been added to your <a href=\"/cart\">shopping cart</a>"))
-                .body("updatetopcartsectionhtml", is("(1)"));
+    void removeItemFromCart() {
+        open("https://demowebshop.tricentis.com/");
+        $(".cart-qty").shouldHave(text("(1)"));
+        $(byText("Shopping cart")).click();
+        $("[name='removefromcart']").click();
+        $(".update-cart-button").click();
+        $(".cart-qty").shouldHave(text("(0)"));
     }
 }
+
+

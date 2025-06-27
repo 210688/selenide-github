86d4691554e4bea0b5ecbc0c7e03e99ad669e6d7
gordi <gordienkoos@krlb.ru>
2025-06-27 06:27:39 +0300

    Написание новых тестов

---
+++ src/test/java/webTests/WebTests.java
@@ -0,0 +1,54 @@
+package webTests;
+
+import com.codeborne.selenide.CollectionCondition;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class WebTests {
+
+    @ValueSource(strings = {"Hello", "World"})
+    @ParameterizedTest(name = "Результаты перевода не пустые для слова {0}")
+    void commonSearchTest(String testData){
+        open("https://translate.yandex.ru/");
+        $("#fakeArea").setValue(testData);
+        $$("#translation").shouldBe(CollectionCondition.sizeGreaterThan(0));
+
+    }
+
+    @CsvSource(value = {
+            "Hello, Здравствуйте",
+            "World, Мир"
+    })
+    @ParameterizedTest(name = "Результаты перевода - \"{1}\" для слова: \"{0}\"")
+    void commonComplexSearchTest(String testData, String expectedResult){
+        open("https://translate.yandex.ru/");
+        $("#fakeArea").setValue(testData);
+        $$("#translation")
+                .first()
+                .shouldHave(text(expectedResult));
+    }
+
+
+    static Stream<Arguments> ioStartMenuTest() {
+        return Stream.of(
+                Arguments.of("EN ", List.of("TESTING EXPERTISE", "FOR YOUR INDUSTRY", "SOLUTIONS", "INSIGHTS & SUCCESS STORIES", "ABOUT US")),
+                Arguments.of("DE ", List.of("SERVICES", "CROWDTESTING", "GERÄTEABDECKUNG", "RESSOURCEN", "COMPANY"))
+        );
+    }
+
+    @MethodSource
+    @ParameterizedTest(name = "Для локали {0} отображаются кнопки {1}")
+    void ioStartMenuTest(String lang, List<String> expectedButtons) {
+        open("https://test.io/");
+        $(".location-selector__button").click();
+        $$(".location-selector__list a").find(text(lang)).click();
+        $$(".top-navigation__row a")
+                .filter(visible)
+                .shouldHave(CollectionCondition.texts(expectedButtons));
+    }
+}
